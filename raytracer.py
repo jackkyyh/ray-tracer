f7e41c443fbf259821ffb1ff59d5ad072fd2280d
jackkyyh <jackkyyh@outlook.com>
2024-04-02 14:07:16 +0000

    extract out rotation function; mask rays before shading

--- raytracer.py
+++ raytracer.py
@@ -1,13 +1,14 @@
 import torch
 from torch.nn.functional import normalize
 
-shaded = lambda im: (im != 0).any(dim=-1)
 
 class Ray:
     def __init__(self, origin, dir):
         self.origin = origin
         self.dir = dir
-        self.dim = origin.shape[:2]
+        self.len = len(origin)
+    def __getitem__(self, mask):
+        return Ray(self.origin[mask], self.dir[mask])
 
 class Scene:
     def __init__(self, background, ambient, objects, lights):
@@ -34,25 +35,22 @@ class Camera:
         width = self.width
         height = self.height
         ppi = self.ppi
+        image_dim = [height*ppi, width*ppi]
 
-        x, y, z = self.target - self.center
-        r = torch.sqrt(x**2 + y**2 + z**2)
-        rr = torch.sqrt(x**2 + y**2)
-        ry = torch.tensor([[c:=rr/r,0,s:=-z/r],[0,1,0],[-s,0,c]])
-        rz = torch.tensor([[c:=x/rr,s:=-y/rr,0],[-s,c,0],[0,0,1]])
+        rotation = rotate(self.target - self.center)
 
-        w = torch.arange(-width/2, width/2, 1/ppi).view([1, -1]).repeat([height*ppi, 1])
-        h = torch.arange(-height/2, height/2, 1/ppi).view([-1, 1]).repeat([1, width*ppi])
-        d = torch.zeros([height*ppi, width*ppi])
-        origin = torch.stack([d, -w, -h], dim=-1)
+        w = torch.arange(-width/2, width/2, 1/ppi).view([1, -1]).expand(image_dim)
+        h = torch.arange(-height/2, height/2, 1/ppi).view([-1, 1]).expand(image_dim)
+        d = torch.zeros(image_dim)
+        origin = torch.stack([d, -w, -h], dim=-1).view([-1, 3])
         eye = torch.tensor([-self.focus, 0, 0])
         dir = origin - eye
-        dir = normalize(dir @ ry.T @ rz.T, dim=-1)
+        dir = normalize(dir @ rotation.T, dim=-1)
 
-        origin = origin @ ry.T @ rz.T
+        origin = origin @ rotation.T
         origin = origin + self.center
 
-        return Ray(origin, dir)
+        return Ray(origin, dir), image_dim
 
 class Object:
     def __init__(self, ambient, diffuse, specular, specular_n):
@@ -61,30 +59,32 @@ class Object:
         self.specular = torch.tensor(specular)
         self.specular_n = torch.tensor(specular_n)
     
-    def shade(self, ray, mask, hit_dist, scene):
+    def shade(self, ray, hit_dist, scene):
         ambient = self.ambient * scene.ambient
 
         light_center = [light.center for light in scene.lights]
-        light_center = torch.stack(light_center, dim=0).unsqueeze(0)
+        light_center = torch.stack(light_center, dim=0).unsqueeze(1)
         light_color = [light.color for light in scene.lights]
-        light_color = torch.stack(light_color, dim=0).unsqueeze(0)
+        light_color = torch.stack(light_color, dim=0).unsqueeze(1)
 
         hit_point = ray.origin + ray.dir * hit_dist.unsqueeze(-1)
-        hit_point = hit_point[mask].unsqueeze(1)
+
+        #[hit, 3]
         normal = self.normal(ray, hit_point)
 
+         #[l, hit, 3]
         to_light = normalize(light_center - hit_point, dim=-1)
-        inner_prod = (normal * to_light).sum(-1).unsqueeze(-1)
+        inner_prod = (normal * to_light).sum(-1, keepdim=True)
         inner_prod[inner_prod < 0] = 0
         diffuse = self.diffuse * inner_prod * light_color
 
         reflection_light = normalize(2 * normal - to_light, dim=-1)
-        inner_prod = (-ray.dir[mask].unsqueeze(1) * reflection_light).sum(dim=-1, keepdim=True)
+        inner_prod = (-ray.dir * reflection_light).sum(dim=-1, keepdim=True)
         inner_prod[inner_prod < 0] = 0
         specular = self.specular * inner_prod**self.specular_n * light_color
 
-        color = torch.zeros([*ray.dim, 3])
-        color[mask] += ambient + diffuse.sum(dim=1) + specular.sum(dim=1)
+        color = torch.zeros([ray.len, 3])
+        color += ambient + diffuse.sum(dim=0) + specular.sum(dim=0)
         return color
 
 class Rect(Object):
@@ -108,11 +108,12 @@ class Rect(Object):
         h_sq = (center_to_hit**2).sum(dim=-1) - w_sq
 
         hit_dist[torch.logical_or(w_sq > self.width**2/4, h_sq > self.height**2/4)] = torch.inf
-        hit_dist[hit_dist < 0] = torch.inf
+        hit_dist[hit_dist < 0.1] = torch.inf
         return hit_dist
 
-    def normal(self, *args):
-        return self.normal_vec
+    def normal(self, ray, *args):
+        sign = -(self.normal_vec * ray.dir).sum(dim=-1, keepdim=True).sgn()
+        return self.normal_vec * sign
 
 class Sphere(Object):
     def __init__(self, center, radius, **kwargs):
@@ -136,24 +137,47 @@ class Sphere(Object):
 
 
 def trace(scene, ray):
+    image = torch.zeros([ray.len, 3])
+    
     z_buffer = [object.intersect(ray) for object in scene.objects]
     z_buffer = torch.stack(z_buffer, dim=0)
     first_hit_idx = torch.argmin(z_buffer, dim=0)
 
-    image = torch.zeros([*ray.dim, 3])
     for idx, obj in enumerate(scene.objects):
-        mask = first_hit_idx == idx
-        mask = torch.logical_and(z_buffer[idx].isinf().logical_not(), mask)
-        shade = obj.shade(ray, mask, z_buffer[idx], scene)
-        assert not torch.logical_and(mask.logical_not(), shaded(shade)).any(), "Shading outside of interection!"
-        image += shade
-
+        is_hit = z_buffer[idx].isinf().logical_not()
+        mask = torch.logical_and(first_hit_idx == idx, is_hit)
+        if mask.count_nonzero() == 0:
+            continue
+        shade = obj.shade(ray[mask], z_buffer[idx][mask], scene)
+        image[mask] += shade
     return image
 
     
 def render(scene, camera):
-    ray = camera.gen_ray()
+    ray, image_dim = camera.gen_ray()
     image = trace(scene, ray)
-    image[shaded(image).logical_not()] = scene.background
+    image[(image == 0).all(dim=-1)] = scene.background
     image[image >= 1] = 1
-    return image
+    return image.view([*image_dim, 3])
+
+
+def rotate(vec):
+    x, y, z = vec.split(1, dim=-1)
+    r = torch.sqrt(x**2 + y**2 + z**2)
+    rr = torch.sqrt(x**2 + y**2)
+    zero = torch.zeros_like(x)
+    one = torch.ones_like(x)
+    
+    c, s = rr / r, -z / r
+    ry = torch.stack([torch.concat([c, zero, s], dim=-1),
+                    torch.concat([zero, one, zero], dim=-1),
+                    torch.concat([-s, zero, c], dim=-1)], dim=-2)
+    
+    c, s = x / rr, y / rr
+    singular = (x**2 + y**2) == 0
+    c[singular] = 1
+    s[singular] = 0
+    rz = torch.stack([torch.concat([c, -s, zero], dim=-1),
+                    torch.concat([s,c, zero], dim=-1),
+                    torch.concat([zero, zero, one], dim=-1)], dim=-2)
+    return rz @ ry
