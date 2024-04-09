b54276cedba7e3bda25f4eb1c318cab623cdd232
JackkyyH <30927793+jackkyyh@users.noreply.github.com>
2024-04-09 15:28:53 +0000

    update scene parameters

--- run.py
+++ run.py
@@ -5,21 +5,21 @@ from raytracer import Camera, Light, Rect, Sphere, Scene
 
 scene = Scene(
     background = [0, 0, 0.],
-    ambient = [0.15, 0.15, 0.15],
+    ambient = [0.1, 0.1, 0.1],
     objects = [
         Rect([80, 0, 0], 160, 140, [0,1,0], [0,0,1],
             intrinsic=[1, 1, 1], diffuse=0.3, specular=0.8, specular_n=50),
         Rect([80, -80, 30], 140, 60, [1,0,0], [0,-1,0],
-            intrinsic=[0.9, 0.4, 0.2], diffuse=0.2, specular=0.9, specular_n=50),
+            intrinsic=[0.7, 0.3, 0.1], diffuse=0.6, specular=0.2, specular_n=50),
         Rect([150, 0, 30], 160, 60, [0,1,0], [1,0,0],
-            intrinsic=[1., 1., 1.], diffuse=0., specular=0.9, specular_n=1e5),
+            intrinsic=[1., 1., 1.], diffuse=0., specular=0.9, specular_n=1e3),
         Sphere([70, 00, 20], 20, 
-            intrinsic=[0.8, 0.8, 1], diffuse=0.8, specular=0.3, specular_n=4),
+            intrinsic=[0.7, 0.7, 1], diffuse=0.8, specular=0.4, specular_n=4),
         Sphere([50, -40, 20], 20, 
             intrinsic=[1, 0.8, 0.0], diffuse=0.6, specular=0.9, specular_n=1e2),
         Sphere([85, 45, 20], 20, 
-            intrinsic=[0.9, 0., 0.6], diffuse=0.1, specular=0.6, specular_n=1e2,
-            refraction=0.7, refraction_index=1.25),
+            intrinsic=[0.9, 0., 0.6], diffuse=0.3, specular=0.5, specular_n=2e2,
+            refraction=0.9, refraction_index=1.25),
 ],
     lights = [Light([30, 20, 100], [1, 1, 1])]
 )
